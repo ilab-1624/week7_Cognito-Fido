165e7fc88fdc8354b6cb9c74936b7ec100c8f62d
BananaDuncan <b10121014@gmail.com>
2021-05-13 04:49:55 +0800

    Update config.py

--- lambda/siginValidation/config.py
+++ lambda/siginValidation/config.py
@@ -1,7 +1,7 @@
 aws_access_key_id = ''
 aws_secret_access_key = ''
 region_name = ''
-sourceBucketName = 'your s3 bucket'
-faceBucketName = 'your s3 bucket'
+sourceBucketName = 'ai-bigdata-system-webapp-face-image'
+faceBucketName = 'ai-bigdata-system-webapp-face-image'
 collectionId = "your face search collectionid"
 table = 'config'
