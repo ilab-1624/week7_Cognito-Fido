cc0055e6df9ea8cbc19031445cdf365471690273
BananaDuncan <b10121014@gmail.com>
2021-05-13 03:45:45 +0800

    Update config.py

--- lambda/siginValidation/config.py
+++ lambda/siginValidation/config.py
@@ -1,6 +1,6 @@
 aws_access_key_id = ''
 aws_secret_access_key = ''
-region_name = 'us-west-2'
+region_name = ''
 sourceBucketName = 'your s3 bucket'
 faceBucketName = 'your s3 bucket'
 collectionId = "your face search collectionid"
