2c8d9f9fa081998bfa2b8e58f5017b8ce3b75a8d
BananaDuncan <b10121014@gmail.com>
2021-05-13 03:37:31 +0800

    Update config.py

--- lambda/siginValidation/config.py
+++ lambda/siginValidation/config.py
@@ -1,7 +1,7 @@
 aws_access_key_id = ''
 aws_secret_access_key = ''
 region_name = 'us-west-2'
-sourceBucketName = 'webapp-registeration-image'
-faceBucketName = 'webapp-registeration-image'
-collectionId = "smartSignIn_Face_Detection"
+sourceBucketName = 'your s3 bucket'
+faceBucketName = 'your s3 bucket'
+collectionId = "your face search collectionid"
 table = 'config'
