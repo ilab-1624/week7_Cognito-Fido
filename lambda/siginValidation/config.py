892eea525965c646884172a1d831bbd6ad30714d
ilab-1624 <68988193+ilab-1624@users.noreply.github.com>
2021-05-12 19:48:12 +0800

    Add files via upload

---
+++ lambda/siginValidation/config.py
@@ -0,0 +1,7 @@
+aws_access_key_id = ''
+aws_secret_access_key = ''
+region_name = 'us-west-2'
+sourceBucketName = 'webapp-registeration-image'
+faceBucketName = 'webapp-registeration-image'
+collectionId = "smartSignIn_Face_Detection"
+table = 'config'
