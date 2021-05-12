8916df08eeb2c0de3568c4bb97e03e64d366c9ed
BananaDuncan <b10121014@gmail.com>
2021-05-13 04:58:59 +0800

    Update config.py

--- lambda/notification/config.py
+++ lambda/notification/config.py
@@ -4,6 +4,5 @@ REGION_NAME = ''   # please fill your region name
 AWS_ACCESS_KEY_ID = ''  # please fill your access key id
 SECRET_ACCESS_KEY = ''  # please fill your secret key
 
-ROLE = ''   # please fill your role that you want to get (manager、staff、customer)
 USER_POOL_ID = ''    # please fill your cognito user pool id
 attributeList = ['custom:lineId']  # user attributes you want to get
